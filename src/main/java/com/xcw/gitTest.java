d3a5348c4d229f35c3ba78dab7f429d5cf89fa21
xcw <3182612051@qq.com>
2022-06-23 16:13:55 +0800

    hot-fix first commit

--- src/main/java/com/xcw/gitTest.java
+++ src/main/java/com/xcw/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.print("hello git");
         System.out.print("hello git1");
+        System.out.print("hello hot-fix 1");
     }
 }
